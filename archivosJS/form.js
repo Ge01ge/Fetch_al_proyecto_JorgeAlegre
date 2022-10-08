43f099f21f420e1a0166a725fffd3f16ca778d82
Ge01ge <106602944+Ge01ge@users.noreply.github.com>
2022-10-08 11:19:45 -0300

    correccion-Detalles

--- archivosJS/form.js
+++ archivosJS/form.js
@@ -32,8 +32,8 @@ function validarFormulario(e) {
     icon: "success",
     html: `<span class="swal-title"> Hola ${nombre}, Gracias por comprar con nosotros. En breve nos contactaremos contigo al tel:${tel} y/o correo:${correo} para definir el modo de pago. Esperamos verte pronto..</span>`,
     background: "rgba(0, 0, 0, 0.265)",
-    showConfirmButton: false,
-    timer: 1500,
+    confirmButtonText: "ok",
+    // showConfirmButton: false,
   });
 }
 
